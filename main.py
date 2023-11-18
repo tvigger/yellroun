0e55443262b33b6a8a1a4e6fe7c33e6d633ec085
tvig <TvigMaster@mail.ru>
2023-11-18 13:53:21 +0300

    random_color

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5 import uic
 from random import randint
 
 
-class YellowCircle(QMainWindow):
+class Window(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.btn = QPushButton(self)
+        self.init_ui()
+
+    def init_ui(self):
+        self.resize(500, 530)
+        self.btn.setGeometry(0, 480, 100, 20)
+        self.btn.setText('Нажми меня')
+
+
+class YellowCircle(Window, QMainWindow):
+    def __init__(self):
+        super().__init__()
         self.setup_ui()
         self.d = 0
 
@@ -29,7 +40,8 @@ class YellowCircle(QMainWindow):
             self.d = 0
 
     def circle(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
+        r, g, b = [randint(0, 255) for _ in range(3)]
+        qp.setBrush(QColor(r, g, b))
         x, y = randint(0, 450), randint(0, 430)
         d = (randint(10, min(500 - x, 480 - y)))
         qp.drawEllipse(x, y, d, d)
